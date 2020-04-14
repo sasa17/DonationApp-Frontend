f37a79891d8f0b8c0d326266939dfd641aa33c2c
lubeina <52536093+lubeina@users.noreply.github.com>
2020-04-14 23:09:45 +0300

    changes made

--- stores/donationStore.js
+++ stores/donationStore.js
@@ -28,11 +28,10 @@ class DonationStore {
 
   checkoutDonation = async (navigation) => {
     this.donations = [];
-    this.checkout_donations = [];
     try {
       const res = await instance.get("checkout/");
       const checkout_donation = res.data.amount;
-      this.checkout_donations = checkout_donation;
+      this.checkout_donations.push(checkout_donation);
       this.loading = false;
       navigation.navigate("List");
     } catch (err) {
@@ -43,7 +42,7 @@ class DonationStore {
   get total() {
     let total = 0.0;
     this.all_donations.forEach((item) => (total += Number(item.amount)));
-    total += Number(this.checkout_donations);
+    this.checkout_donations.forEach((item) => (total += Number(item)));
     console.log("donationsss", total);
     return total;
   }
